cdfa1097f7f1db3d9626279e68bb3b44074d0d7a
Suleyman Karaman <suleymann.krmn@gmail.com>
2026-02-18 15:01:37 +0300

    Telefonda alt bar fixlendi

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,15 @@
-import React, { useState } from 'react';
+import * as NavigationBar from 'expo-navigation-bar';
+import React, { useEffect, useState } from 'react';
 import GameScreen from '../../src/screens/GameScreen';
 import HomeScreen from '../../src/screens/HomeScreen';
 
 export default function Index() {
   const [screen, setScreen] = useState('home');
+
+  useEffect(() => {
+    NavigationBar.setVisibilityAsync('hidden');
+    NavigationBar.setBehaviorAsync('overlay-swipe');
+  }, []);
 
   if (screen === 'game') {
     return <GameScreen onHome={() => setScreen('home')} />;
